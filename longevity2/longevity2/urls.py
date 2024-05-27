850e16969843b6d3f2d48d7f46ef5569fee47fb2
Artur Gayfutdinov <gaifut@gmail.com>
2024-05-27 11:07:40 +0300

    Update urls.py
    replace url with re_path

--- longevity2/longevity2/urls.py
+++ longevity2/longevity2/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls import url
+from django.urls import include, path, repath
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -26,10 +25,10 @@ urlpatterns = [
 ]
 
 urlpatterns += [
-   url(r'^swagger(?P<format>\.json|\.yaml)$', 
+   re_path(r'^swagger(?P<format>\.json|\.yaml)$', 
        schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
+   re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), 
        name='schema-swagger-ui'),
-   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
+   re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), 
        name='schema-redoc'),
 ]
